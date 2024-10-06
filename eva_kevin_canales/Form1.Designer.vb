16b56603202df5b118e9cdd4b8403fc66fab6e54
KevinCG1423 <kevin.canales.gonzalez@gmail.com>
2024-10-06 00:59:08 -0300

    Agregar archivos de proyecto.

---
+++ eva_kevin_canales/Form1.Designer.vb
@@ -0,0 +1,312 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        btnBuscar = New Button()
+        btnGuardar = New Button()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        ComboBox1 = New ComboBox()
+        btnActualizar = New Button()
+        btnEliminar = New Button()
+        btnVerUsuarios = New Button()
+        grpUsuarios = New GroupBox()
+        grpUsuarios.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(168, 42)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(197, 27)
+        TextBox1.TabIndex = 0
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(168, 100)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(197, 27)
+        TextBox2.TabIndex = 1
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(168, 158)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(197, 27)
+        TextBox3.TabIndex = 2
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(168, 362)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(197, 27)
+        TextBox5.TabIndex = 3
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(168, 412)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(197, 27)
+        TextBox6.TabIndex = 4
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(71, 42)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(43, 20)
+        Label1.TabIndex = 5
+        Label1.Text = "RUT :"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(199, 9)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(147, 20)
+        Label2.TabIndex = 6
+        Label2.Text = "Ejemplo : 224231121"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(71, 103)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(71, 20)
+        Label3.TabIndex = 7
+        Label3.Text = "Nombre :"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(71, 161)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(73, 20)
+        Label4.TabIndex = 8
+        Label4.Text = "Apellido :"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(71, 362)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(56, 20)
+        Label5.TabIndex = 9
+        Label5.Text = "Cuidad"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(71, 316)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(64, 20)
+        Label6.TabIndex = 10
+        Label6.Text = "Comuna"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(53, 412)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(91, 20)
+        Label7.TabIndex = 11
+        Label7.Text = "Observacion"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(50, 210)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(48, 20)
+        Label8.TabIndex = 12
+        Label8.Text = "Sexo :"
+        ' 
+        ' btnBuscar
+        ' 
+        btnBuscar.Location = New Point(421, 31)
+        btnBuscar.Name = "btnBuscar"
+        btnBuscar.Size = New Size(120, 49)
+        btnBuscar.TabIndex = 13
+        btnBuscar.Text = "Buscar Usuario"
+        btnBuscar.UseVisualStyleBackColor = True
+        ' 
+        ' btnGuardar
+        ' 
+        btnGuardar.Location = New Point(101, 17)
+        btnGuardar.Name = "btnGuardar"
+        btnGuardar.Size = New Size(157, 53)
+        btnGuardar.TabIndex = 14
+        btnGuardar.Text = "Guardar Usuario"
+        btnGuardar.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(71, 250)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(97, 24)
+        RadioButton1.TabIndex = 15
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Masculino"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(213, 250)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(95, 24)
+        RadioButton2.TabIndex = 16
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Femenino"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(356, 250)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(121, 24)
+        RadioButton3.TabIndex = 17
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "No especifico"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(168, 313)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(197, 28)
+        ComboBox1.TabIndex = 18
+        ' 
+        ' btnActualizar
+        ' 
+        btnActualizar.Location = New Point(281, 17)
+        btnActualizar.Name = "btnActualizar"
+        btnActualizar.Size = New Size(140, 53)
+        btnActualizar.TabIndex = 19
+        btnActualizar.Text = "Actualizar"
+        btnActualizar.UseVisualStyleBackColor = True
+        ' 
+        ' btnEliminar
+        ' 
+        btnEliminar.Location = New Point(101, 82)
+        btnEliminar.Name = "btnEliminar"
+        btnEliminar.Size = New Size(157, 59)
+        btnEliminar.TabIndex = 20
+        btnEliminar.Text = "Eliminar"
+        btnEliminar.UseVisualStyleBackColor = True
+        ' 
+        ' btnVerUsuarios
+        ' 
+        btnVerUsuarios.Location = New Point(281, 74)
+        btnVerUsuarios.Name = "btnVerUsuarios"
+        btnVerUsuarios.Size = New Size(140, 67)
+        btnVerUsuarios.TabIndex = 21
+        btnVerUsuarios.Text = "Ver Todos los Usuarios"
+        btnVerUsuarios.UseVisualStyleBackColor = True
+        ' 
+        ' grpUsuarios
+        ' 
+        grpUsuarios.Controls.Add(btnActualizar)
+        grpUsuarios.Controls.Add(btnVerUsuarios)
+        grpUsuarios.Controls.Add(btnGuardar)
+        grpUsuarios.Controls.Add(btnEliminar)
+        grpUsuarios.Location = New Point(31, 452)
+        grpUsuarios.Name = "grpUsuarios"
+        grpUsuarios.Size = New Size(446, 143)
+        grpUsuarios.TabIndex = 22
+        grpUsuarios.TabStop = False
+        grpUsuarios.Text = "Gestión de Usuarios"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(575, 607)
+        Controls.Add(grpUsuarios)
+        Controls.Add(ComboBox1)
+        Controls.Add(RadioButton3)
+        Controls.Add(RadioButton2)
+        Controls.Add(RadioButton1)
+        Controls.Add(btnBuscar)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox6)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        grpUsuarios.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents btnActualizar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents btnVerUsuarios As Button
+    Friend WithEvents grpUsuarios As GroupBox
+
+End Class
---
+++ eva_kevin_canales/Form1.vb
@@ -0,0 +1,219 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Private connectionString As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        connectionString = "Server=localhost;Database=registropersonas;Uid=root;Pwd=;"
+        LoadComunAs()
+    End Sub
+
+    Private Sub LoadComunAs()
+        Using conn As New MySqlConnection(connectionString)
+            Dim cmd As New MySqlCommand("SELECT NombreComuna FROM comuna", conn)
+            Try
+                conn.Open()
+                Dim reader As MySqlDataReader = cmd.ExecuteReader()
+                While reader.Read()
+                    ComboBox1.Items.Add(reader("NombreComuna").ToString())
+                End While
+            Catch ex As Exception
+                MessageBox.Show("Error al cargar comunas: " & ex.Message)
+            Finally
+                conn.Close()
+            End Try
+        End Using
+    End Sub
+
+    Private Function ValidateFields() As Boolean
+        If String.IsNullOrWhiteSpace(TextBox1.Text) OrElse
+           String.IsNullOrWhiteSpace(TextBox2.Text) OrElse
+           String.IsNullOrWhiteSpace(TextBox3.Text) OrElse
+           ComboBox1.SelectedItem Is Nothing Then
+            MessageBox.Show("Por favor, complete todos los campos obligatorios.")
+            Return False
+        End If
+        Return True
+    End Function
+
+    Private Sub ClearFields()
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        RadioButton1.Checked = False
+        RadioButton2.Checked = False
+        RadioButton3.Checked = False
+        ComboBox1.SelectedIndex = -1
+        TextBox5.Clear()
+        TextBox6.Clear()
+        TextBox1.Focus()
+    End Sub
+
+    ' Guardar usuario
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        If ValidateFields() Then
+            Dim rut = TextBox1.Text
+            Dim nombre = TextBox2.Text
+            Dim apellido = TextBox3.Text
+            Dim sexo = If(RadioButton1.Checked, "Masculino", If(RadioButton2.Checked, "Femenino", "No especifica"))
+            Dim comuna = ComboBox1.SelectedItem.ToString()
+            Dim ciudad = TextBox5.Text
+            Dim observacion = TextBox6.Text
+
+            Using conn As New MySqlConnection(connectionString)
+                Dim cmd As New MySqlCommand("INSERT INTO personas (RUT, Nombre, Apellido, Sexo, Comuna, Ciudad, Observacion) VALUES (@RUT, @Nombre, @Apellido, @Sexo, @Comuna, @Ciudad, @Observacion)", conn)
+                cmd.Parameters.AddWithValue("@RUT", rut)
+                cmd.Parameters.AddWithValue("@Nombre", nombre)
+                cmd.Parameters.AddWithValue("@Apellido", apellido)
+                cmd.Parameters.AddWithValue("@Sexo", sexo)
+                cmd.Parameters.AddWithValue("@Comuna", comuna)
+                cmd.Parameters.AddWithValue("@Ciudad", ciudad)
+                cmd.Parameters.AddWithValue("@Observacion", observacion)
+
+                Try
+                    conn.Open()
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Registro guardado exitosamente.")
+                    ClearFields()
+                Catch ex As Exception
+                    MessageBox.Show("Error al guardar registro: " & ex.Message)
+                Finally
+                    conn.Close()
+                End Try
+            End Using
+        End If
+    End Sub
+
+    ' Buscar usuario
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        Dim rut = TextBox1.Text
+
+        If String.IsNullOrWhiteSpace(rut) Then
+            MessageBox.Show("Por favor, ingrese un RUT para buscar.")
+            Return
+        End If
+
+        Using conn As New MySqlConnection(connectionString)
+            Dim cmd As New MySqlCommand("SELECT * FROM personas WHERE RUT = @RUT", conn)
+            cmd.Parameters.AddWithValue("@RUT", rut)
+
+            Try
+                conn.Open()
+                Dim reader = cmd.ExecuteReader()
+                If reader.Read() Then
+                    TextBox2.Text = reader("Nombre").ToString()
+                    TextBox3.Text = reader("Apellido").ToString()
+                    If reader("Sexo").ToString() = "Masculino" Then
+                        RadioButton1.Checked = True
+                    ElseIf reader("Sexo").ToString() = "Femenino" Then
+                        RadioButton2.Checked = True
+                    Else
+                        RadioButton3.Checked = True
+                    End If
+                    ComboBox1.SelectedItem = reader("Comuna").ToString()
+                    TextBox5.Text = reader("Ciudad").ToString()
+                    TextBox6.Text = reader("Observacion").ToString()
+                Else
+                    MessageBox.Show("RUT no encontrado.")
+                End If
+            Catch ex As Exception
+                MessageBox.Show("Error al buscar registro: " & ex.Message)
+            Finally
+                conn.Close()
+            End Try
+        End Using
+    End Sub
+
+    ' Actualizar usuario
+    Private Sub btnActualizar_Click(sender As Object, e As EventArgs) Handles btnActualizar.Click
+        If ValidateFields() Then
+            Dim rut = TextBox1.Text
+            Dim nombre = TextBox2.Text
+            Dim apellido = TextBox3.Text
+            Dim sexo = If(RadioButton1.Checked, "Masculino", If(RadioButton2.Checked, "Femenino", "No especifica"))
+            Dim comuna = ComboBox1.SelectedItem.ToString()
+            Dim ciudad = TextBox5.Text
+            Dim observacion = TextBox6.Text
+
+            Using conn As New MySqlConnection(connectionString)
+                Dim cmd As New MySqlCommand("UPDATE personas SET Nombre = @Nombre, Apellido = @Apellido, Sexo = @Sexo, Comuna = @Comuna, Ciudad = @Ciudad, Observacion = @Observacion WHERE RUT = @RUT", conn)
+                cmd.Parameters.AddWithValue("@RUT", rut)
+                cmd.Parameters.AddWithValue("@Nombre", nombre)
+                cmd.Parameters.AddWithValue("@Apellido", apellido)
+                cmd.Parameters.AddWithValue("@Sexo", sexo)
+                cmd.Parameters.AddWithValue("@Comuna", comuna)
+                cmd.Parameters.AddWithValue("@Ciudad", ciudad)
+                cmd.Parameters.AddWithValue("@Observacion", observacion)
+
+                Try
+                    conn.Open()
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Registro actualizado exitosamente.")
+                    ClearFields()
+                Catch ex As Exception
+                    MessageBox.Show("Error al actualizar registro: " & ex.Message)
+                Finally
+                    conn.Close()
+                End Try
+            End Using
+        End If
+    End Sub
+
+    ' Eliminar usuario
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        Dim rut = TextBox1.Text
+
+        If String.IsNullOrWhiteSpace(rut) Then
+            MessageBox.Show("Por favor, ingrese un RUT para eliminar.")
+            Return
+        End If
+
+        If MessageBox.Show("¿Está seguro de que desea eliminar este usuario?", "Confirmar eliminación", MessageBoxButtons.YesNo) = DialogResult.Yes Then
+            Using conn As New MySqlConnection(connectionString)
+                Dim cmd As New MySqlCommand("DELETE FROM personas WHERE RUT = @RUT", conn)
+                cmd.Parameters.AddWithValue("@RUT", rut)
+
+                Try
+                    conn.Open()
+                    Dim rowsAffected = cmd.ExecuteNonQuery()
+                    If rowsAffected > 0 Then
+                        MessageBox.Show("Usuario eliminado exitosamente.")
+                    Else
+                        MessageBox.Show("RUT no encontrado.")
+                    End If
+                Catch ex As Exception
+                    MessageBox.Show("Error al eliminar registro: " & ex.Message)
+                Finally
+                    conn.Close()
+                End Try
+            End Using
+        End If
+    End Sub
+
+    ' Ver todos los usuarios
+    Private Sub btnVerUsuarios_Click(sender As Object, e As EventArgs) Handles btnVerUsuarios.Click
+        Using conn As New MySqlConnection(connectionString)
+            Dim cmd As New MySqlCommand("SELECT RUT, Nombre, Apellido FROM personas", conn)
+
+            Try
+                conn.Open()
+                Dim reader = cmd.ExecuteReader()
+                Dim usuarios As String = "Lista de Usuarios:" & Environment.NewLine
+
+                While reader.Read()
+                    usuarios &= $"{reader("RUT")} - {reader("Nombre")} {reader("Apellido")}" & Environment.NewLine
+                End While
+
+                If usuarios = "Lista de Usuarios:" & Environment.NewLine Then
+                    usuarios = "No hay usuarios registrados."
+                End If
+
+                MessageBox.Show(usuarios)
+            Catch ex As Exception
+                MessageBox.Show("Error al obtener la lista de usuarios: " & ex.Message)
+            Finally
+                conn.Close()
+            End Try
+        End Using
+    End Sub
+End Class
